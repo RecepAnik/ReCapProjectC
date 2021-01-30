3e7e5386b5d42871ad49012c4ec545ecdd673d47
RecepAnik <anik.recep@gmail.com>
2021-01-31 00:05:23 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,30 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.InMemory;
+using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            ProductManager productManager = new ProductManager(new InMemoryProductDal());
+
+            foreach (var product in productManager.GetAll()) 
+            {
+                Console.WriteLine(product.ProdcutName);
+
+            }
+            Console.WriteLine("---");
+
+
+            foreach (var product in productManager.GetAll())
+            {
+                Console.WriteLine(product.UnitPrice);
+
+            }
+        }
+    }
+}
